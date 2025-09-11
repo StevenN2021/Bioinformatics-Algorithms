2df9d5e24dc7505a5423d5e8d360fa47fc348c25
StevenN2021 <117956466+StevenN2021@users.noreply.github.com>
2025-09-11 02:04:22 +0000

    started find clumps

---
+++ Chapter1/findclumps.py
@@ -0,0 +1,12 @@
+import sys
+
+'''
+Input: A string Genome, and integers k, L, and t.
+Output: All distinct k-mers forming (L, t)-clumps in Genome.
+'''
+
+def find_clumps(genome: str, k: int, l: int, t: int) -> list[str]:
+    """Find patterns forming clumps in a genome."""
+
+def freq_table(window: str, k: int) -> dict
+    
