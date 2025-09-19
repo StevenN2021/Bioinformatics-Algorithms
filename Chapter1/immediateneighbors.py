61e7f439af97464b1f35b7a58faee97ae12aaa7f
StevenN2021 <117956466+StevenN2021@users.noreply.github.com>
2025-09-19 08:05:17 -0700

    started neighbors function

---
+++ Chapter1/immediateneighbors.py
@@ -0,0 +1,22 @@
+'''
+Input: A string Pattern and an integer d.
+Output: The collection of strings Neighbors(Pattern, d).
+'''
+
+def neighbors(s: str, d: int) -> list[str]:
+  """Generate neighbors of a string within a given Hamming distance."""
+  res = set()
+  res.add(s)
+
+  d = {
+    "A" : ["T","C","G"],
+    "T" : ["A","C","G"],
+    "C" : ["T","A","G"],
+    "G" : ["T","C","A"]
+  }
+  for i in range(len(s)):
+    symbol = s[i]
+    
+  
+  
+  
