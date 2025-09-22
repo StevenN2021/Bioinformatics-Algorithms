43743d97fc5737662d34feb10811b384b9d1c818
StevenN2021 <117956466+StevenN2021@users.noreply.github.com>
2025-09-22 07:54:04 -0700

    completed immediateneighbors.py

--- Chapter1/immediateneighbors.py
+++ Chapter1/immediateneighbors.py
@@ -4,24 +4,34 @@ Output: The collection of strings Neighbors(Pattern, d).
 '''
 
 def neighbors(s: str, d: int) -> list[str]:
-  """Generate neighbors of a string within a given Hamming distance."""
-  res = set()
-  res.add(s)
-
-  d = {
-    "A" : ["T","C","G"],
-    "T" : ["A","C","G"],
-    "C" : ["T","A","G"],
-    "G" : ["T","C","A"]
-  }
-  for i in range(len(s)):
-    symbol = s[i]
-    curr = list(s) 
-    for x in d[symbol]: 
-      curr[i] = x
-      res.add(curr) 
-  return res
-      
+    """Generate neighbors of a string within a given Hamming distance."""
+    
+    # base case 
+    if d == 0:
+        return {s}
+    if len(s) == 1:
+        return [s] if d == 0 else ["A","C","T","G"]
+    
+    res = set()
+    suffix_neighbors = neighbors(s[1:],d) 
+    
+    for text in suffix_neighbors:
+        if hamming(s[1:],text) < d: 
+            for nuc in ["A","T","C","G"]:
+                res.add(nuc + text) 
+        else:
+            res.add(s[0] + text)
+              
+    return res
+    
+ 
+    
+def hamming(s: str, t: str) -> int:
+    count = 0
+    for i in range(len(s)):
+        if s[i] != t[i]:
+            count+=1
+    return count
       
     
     
