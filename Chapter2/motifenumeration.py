26bbad1a79809db2003586861394665bb7842721
StevenN2021 <117956466+StevenN2021@users.noreply.github.com>
2025-09-23 15:04:09 +0000

    started motifenumeration

--- Chapter2/motifenumeration.py
+++ Chapter2/motifenumeration.py
@@ -1,1 +1,42 @@
 
+def MotifEnumeration(Dna: list[str], k: int, d: int) -> list[str]:
+    patterns = set()
+    for seq in Dna: 
+        for i in range(len(seq)-k+1):
+            pattern = seq[i:i+k]
+            neighbors = Neighbors(pattern,d)
+            for neighbor in neighbors: 
+                if hamming(neighbor,pattern) <= 2:
+                    
+
+
+def Neighbors(s: str, d: int) -> list[str]:
+    """Generate neighbors of a string within a given Hamming distance."""
+    
+    # base case 
+    if d == 0:
+        return {s}
+    if len(s) == 1:
+        return [s] if d == 0 else ["A","C","T","G"]
+    
+    res = set()
+    suffix_neighbors = Neighbors(s[1:],d) 
+    
+    for text in suffix_neighbors:
+        if hamming(s[1:],text) < d: 
+            for nuc in ["A","T","C","G"]:
+                res.add(nuc + text) 
+        else:
+            res.add(s[0] + text)
+              
+    return res
+    
+ 
+    
+def hamming(s: str, t: str) -> int:
+    count = 0
+    for i in range(len(s)):
+        if s[i] != t[i]:
+            count+=1
+    return count
+      
